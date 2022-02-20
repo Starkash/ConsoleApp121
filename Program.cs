460a1a2f18abca5a9affbcf1e870edcd7db279f0
HP <HP@DESKTOP-POHI4N8>
2022-02-20 14:20:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Sec
+{
+    class NewApp
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= 6 - i; j++)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int k = 1; k <= i + 1; k++)
+                    Console.Write("*");
+
+
+            }
+            Console.ReadLine();
+
+        }
+        
+    }
+}
